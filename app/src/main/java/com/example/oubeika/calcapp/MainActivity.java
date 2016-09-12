676e5f28757d6d9996475952eff8d0883a97d8e2
igokichi1515 <igokichi1515@i.softbank.jp>
2016-09-12 15:48:40 +0900

    edittextに数値が未入力の場合の処理を追加

--- app/src/main/java/com/example/oubeika/calcapp/MainActivity.java
+++ app/src/main/java/com/example/oubeika/calcapp/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.oubeika.calcapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,30 +31,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         minus.setOnClickListener(this);
         kakeru.setOnClickListener(this);
         waru.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View view) {
 
         double result = 0;
-        double num1 = Double.parseDouble(edit1.getText().toString());
-        double num2 = Double.parseDouble(edit2.getText().toString());
+        double num1;
+        double num2;
 
-        if(view != null){
-            if(view.getId() == R.id.plus){
-                result = num1 + num2;
-            }else if(view.getId() == R.id.minus){
-                result = num1 - num2;
-            }else if(view.getId() == R.id.kakeru){
-                result = num1 * num2;
-            }else if(view.getId() == R.id.waru){
-                result = num1 / num2;
+        if(edit1.length() != 0 && edit2.length() != 0){
+            num1 = Double.parseDouble(edit1.getText().toString());
+            num2 = Double.parseDouble(edit2.getText().toString());
+
+            if(view != null){
+                if(view.getId() == R.id.plus){
+                    result = num1 + num2;
+                }else if(view.getId() == R.id.minus){
+                    result = num1 - num2;
+                }else if(view.getId() == R.id.kakeru){
+                    result = num1 * num2;
+                }else if(view.getId() == R.id.waru){
+                    result = num1 / num2;
+                }
             }
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("RESULT", String.valueOf(result));
+            startActivity(intent);
+        }else{
+            Toast.makeText(MainActivity.this, "数値を入力してください。",
+                    Toast.LENGTH_SHORT).show();
         }
-
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("RESULT", String.valueOf(result));
-        startActivity(intent);
     }
 }
